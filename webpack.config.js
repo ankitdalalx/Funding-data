c0ac621d262865b653ffe6a72ff073f9c08d2742
cypherhack99 <58860117+cypherhack99@users.noreply.github.com>
2024-08-09 23:33:54 +0530

    Pulish last

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.js', // Set the entry point to your index.js file
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.js$/, // Adjusted to test for .js files instead of .ts/.tsx files
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Use Babel to transpile JS files if needed
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.js'], // Resolve JavaScript files by default
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // The output bundle name
+    path: path.resolve(__dirname, 'dist'), // The output directory
   },
 };
